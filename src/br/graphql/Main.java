de8d7c72bc7c1702a609f54fe49ff24633961456
Windson Viana <windson.viana@tjdft.jus.br>
2020-10-21 22:54:27 -0300

    tentativa jwt

--- src/br/graphql/Main.java
+++ src/br/graphql/Main.java
@@ -7,18 +7,17 @@ import org.springframework.web.client.RestTemplate;
 
 public class Main {
 	public static void main(String[] args) {
-		String uri="http://localhost:8080/graphql";
-		String query = "query{\n"
-				+ "       estagiario(login:\"e706350\"){\n"
-				+ "         id\n"
-				+ "         login\n"
-				+ "         nome\n"
-				+ "      }\n"
-				+ "}";
+		String uri="http://localhost:8000/graphql";
+		String query = "query {\n"
+				+ "       emails{\n"
+				+ "         title\n"
+				+ "         body\n"
+				+ "       }\n"
+				+ "     }";
 		
 		RestTemplate restTemplate = new RestTemplate();
 	    HttpHeaders headers = new HttpHeaders();
-	    //headers.add("Authorization", "Bearer *************");
+	    headers.add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJyb2xlcyI6ImFkbWluLGEsYiJ9.M6-2eMXB7IUhZOVJUBfobAOwX0j7Zf99I3ltvsvo2tM");
 	    headers.add("content-type", "application/graphql");
 
 	    ResponseEntity<String> response = 
